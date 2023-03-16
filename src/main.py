be93b0a2f9f933626d299502af481cb9b923ef52
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-16 21:24:19 +0000

    made the program interactive rather than hardcoding data

--- src/main.py
+++ src/main.py
@@ -1,18 +1,38 @@
 from category import Category
+import time
 
 groceries = Category('Groceries')
 entertainment = Category('Entertainment')
 python_tutorials = Category('Python Tutorials')
 
-groceries.deposit(500, "First Deposit")
+print("Welcome to Python Bank and Trust!")
+time.sleep(2)
+
+initial_deposit = input("Would you like to make a deposit? ").capitalize()
+
+if initial_deposit == 'Yes':
+    pass
+else:
+    time.sleep(1)
+    print("Thank you for banking with us, have a nice day!")
+    time.sleep(1)
+    quit()
+
+groceries.deposit(int(input("How much would you like to deposit for Groceries? ")), "First Deposit")
+time.sleep(1)
 # print(groceries.get_balance())
-groceries.withdrawl(100, "Got groceries")
+groceries.withdrawl(int(input("How much would you like to take out for Groceries? ")), "Got groceries")
+time.sleep(1)
 # print(groceries.get_balance())
-groceries.transfer(50, python_tutorials)
+groceries.transfer(int(input("How much would you like to transfer to your Python Tutorials fund? ")), python_tutorials)
+time.sleep(1)
 # print(python_tutorials.get_balance())
-python_tutorials.withdrawl(45)
-entertainment.deposit(150, "Second Deposit")
-entertainment.withdrawl(75)
+python_tutorials.withdrawl(int(input("How much would you like to take out from your Python Tutorials fund? ")))
+time.sleep(1)
+entertainment.deposit(int(input("How much would you like to deposit for Entertainment?? ")), "Second Deposit")
+time.sleep(1)
+entertainment.withdrawl(int(input("How much would you like to take out from your Entertainment fund? ")))
+time.sleep(1)
 
 print(groceries)
 print(python_tutorials)
