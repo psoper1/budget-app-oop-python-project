6a811390fd7cd1db6b628bd8bcfbeb36136358ce
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-16 19:16:04 +0000

    technically finished, don't quite understand the syntax used in __str__, found that part, need to study more on it

--- src/category.py
+++ src/category.py
@@ -2,14 +2,44 @@ class Category:
     def __init__(self, name):
         self.name = name
         self.ledger = []
-        self.amount = 0
-    def withdrawl(self):
-        pass
-    def deposit(self):
-        pass
+
+    def withdrawl(self, amount, description=""):
+        if self.check_funds(amount) == False:
+            return False
+        else:
+            self.ledger.append({"amount": amount * -1, "description": description})
+            return True
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
     def get_balance(self):
-        pass
+        money_in_bank = 0
+        for transaction in self.ledger:
+            money_in_bank = money_in_bank + transaction["amount"]
+        return money_in_bank
+
+    def transfer(self, amount, group):
+        if not self.withdrawl(amount, f"Transfer to {group.name}"):
+            return False
+        else:
+            group.deposit(amount, f"Transfer from {self.name}")
+            return True
+
+    def check_funds(self, amount):
+        if self.get_balance() < amount:
+            return False
+        else:
+            return True
+
     def __str__(self):
-        pass
-    def transfer(self):
-        pass
+        title = f"{self.name:*^30}\n"
+        items = ""
+        total = 0
+        for item in self.ledger:
+            items = items + f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
+
+            total = total + item['amount']
+
+        output = title + items + "Total: " + str(total)
+        return output
--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 from category import Category
 
-food = Category('food')
-groceries = Category('groceries')
-entertainment = Category('entertainment')
+groceries = Category('Groceries')
+entertainment = Category('Entertainment')
+python_tutorials = Category('Python Tutorials')
+
+groceries.deposit(500, "First Deposit")
+# print(groceries.get_balance())
+groceries.withdrawl(100, "Got groceries")
+# print(groceries.get_balance())
+groceries.transfer(50, python_tutorials)
+# print(python_tutorials.get_balance())
+python_tutorials.withdrawl(45)
+entertainment.deposit(150, "Second Deposit")
+entertainment.withdrawl(75)
+
+print(groceries)
+print(python_tutorials)
