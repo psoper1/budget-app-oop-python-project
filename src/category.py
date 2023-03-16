773f2ec0827cd057bb05f232c12cdae7700b3b36
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-16 15:28:14 +0000

    not much progress

---
+++ src/category.py
@@ -0,0 +1,15 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.amount = 0
+    def withdrawl(self):
+        pass
+    def deposit(self):
+        pass
+    def get_balance(self):
+        pass
+    def __str__(self):
+        pass
+    def transfer(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from category import Category
+
+food = Category('food')
+groceries = Category('groceries')
+entertainment = Category('entertainment')
