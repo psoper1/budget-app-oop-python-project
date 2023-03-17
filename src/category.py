91a2f9eb8c6d89e1598d97359c7f0b93517e9a76
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-17 20:49:17 +0000

    Added some more comments to explain what is going on

--- src/category.py
+++ src/category.py
@@ -1,25 +1,33 @@
+# Create a class called Category
 class Category:
     def __init__(self, name):
+        # Create attributes for the name and the ledger list
         self.name = name
         self.ledger = []
 
     def withdrawl(self, amount, description=""):
+        # Is supposed to fail the withdrawl if the amount be withdrawn is more than what is available
         if self.check_funds(amount) == False:
             return False
         else:
+            # I thought this was clever multiplying the amount by -1 to show it like it's being minued
             self.ledger.append({"amount": amount * -1, "description": description})
             return True
 
     def deposit(self, amount, description=""):
+        # Add money to the ledger list
         self.ledger.append({"amount": amount, "description": description})
 
     def get_balance(self):
+        # Set the initial value of the balance to 0
         money_in_bank = 0
+        # Loop to check the ledger and return an updated amount after any deposits / withdrawls
         for transaction in self.ledger:
             money_in_bank = money_in_bank + transaction["amount"]
         return money_in_bank
 
     def transfer(self, amount, group):
+        # Checks and transfers money from one account to another, pretty limited for the moment
         if not self.withdrawl(amount, f"Transfer to {group.name}"):
             return False
         else:
@@ -27,6 +35,7 @@ class Category:
             return True
 
     def check_funds(self, amount):
+        # Checks how much money based on the updated balance
         if self.get_balance() < amount:
             return False
         else:
--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+# Import the category class and any other methods I may need
+
 from category import Category
 import time
 
+# Hardcode some initial values
 groceries = Category('Groceries')
 entertainment = Category('Entertainment')
 python_tutorials = Category('Python Tutorials')
 
+# Prints the opening since this is semi user driven
 print("Welcome to Python Bank and Trust!")
 time.sleep(2)
 
+# Generic true or false to start the process
 initial_deposit = input("Would you like to make a deposit? ").capitalize()
 
 if initial_deposit == 'Yes':
@@ -17,6 +22,9 @@ else:
     print("Thank you for banking with us, have a nice day!")
     time.sleep(1)
     quit()
+
+# Questions that take the user through depositing and moving money and withdrawing money
+# Mostly hardcoded but is a semi-trick...
 
 groceries.deposit(int(input("How much would you like to deposit for Groceries? ")), "First Deposit")
 time.sleep(1)
@@ -33,6 +41,8 @@ entertainment.deposit(int(input("How much would you like to deposit for Entertai
 time.sleep(1)
 entertainment.withdrawl(int(input("How much would you like to take out from your Entertainment fund? ")))
 time.sleep(1)
+
+# Prints a couple of the categories to show on the receipt
 
 print(groceries)
 print(python_tutorials)
